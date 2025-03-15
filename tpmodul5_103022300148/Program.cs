ab79665bfa7e34d31a19b4f44a709ff589d0cbf6
Yappier Albertus <yappier.albert1@gmail.com>
2025-03-15 20:21:00 +0700

    menambahkan DataGeneric

--- tpmodul5_103022300148/Program.cs
+++ tpmodul5_103022300148/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class DataGeneric<T>(T data) {
+    private T data = data;
+
+    public void PrintData() {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+public class Program() { 
+    public static void Main()
+    {
+        DataGeneric<long> data = new DataGeneric<long>(103022300148);
+        data.PrintData();
+    }   
+}
