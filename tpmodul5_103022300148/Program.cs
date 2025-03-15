130934f032a9c83d1a946871c60b5f0cb2937d5c
Yappier Albertus <yappier.albert1@gmail.com>
2025-03-15 20:01:41 +0700

    menambahkan HaloGeneric

--- tpmodul5_103022300148/Program.cs
+++ tpmodul5_103022300148/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class HaloGeneric() {
+    public void SapaUser<T>(T x) {
+        Console.WriteLine("Halo User " + x);
+    }
+}
+
+public class Program() {
+    public static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        String x = Console.ReadLine();
+        halo.SapaUser(x);
+    }
+}
